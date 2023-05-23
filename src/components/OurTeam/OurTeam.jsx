858a8c0ffb1dac5d7e6824912e3fa391ce2218ac
Juan Ignacio Garcia <nachogarcia.1017@gmail.com>
2023-05-23 18:34:16 -0300

    minor fixes

--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -1,35 +1,40 @@
 const OurTeam = () => {
-    return (
-        <div className="ourTeamSection">
-            <div className="ourTeamSection__box1">
-                <h1 className="ourTeamSection__box1__title">OurTeam</h1>
-                <p className="ourTeamSection__box1__pharagrap">
-                    At zolution, we have a highly talented and dedicated team that works together to deliver exceptional web solutions. Our
-                    team is made up of experts in web development, creative designers and professionals who are passionate about the digital
-                    world. Nicolás Massaccesi - Frontend Developer With 2 years of experience in web development, Nicolás Massaccesi is our
-                    technical leader and expert in all areas of web development. His deep knowledge of programming languages and meticulous
-                    approach ensure that each project is executed with precision and efficiency. Juan I. García - Frontend Developer With 2
-                    years of experience in web development, Juan I. García is our technical leader and expert in all areas of web
-                    development. His deep knowledge of programming languages and meticulous approach ensure that each project is executed
-                    with precision and efficiency.
-                </p>
-            </div>
-            <div className="ourTeamSection__box2">
-                <img
-                    className="ourTeamSection__img1"
-                    src="https://res.cloudinary.com/dwavbmsb2/image/upload/v1684870874/977337284d108765cc0957631f768e10_air9aw.jpg"
-                    alt="Foto Nico Massaccesi"
-                />
-            </div>
-            <div className="ourTeamSection__box3">
-                <img
-                    className="ourTeamSection__img2"
-                    src="https://res.cloudinary.com/dwavbmsb2/image/upload/v1684870875/b425762e454989f9014bfb463fac91b0_tsc26z.png"
-                    alt="Foto Juani Garcia"
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="ourTeamSection">
+      <div className="ourTeamSection__box1">
+        <h1 className="ourTeamSection__box1__title">Our Team</h1>
+        <p className="ourTeamSection__box1__pharagrap">
+          At zolution, we have a highly talented and dedicated team that works
+          together to deliver exceptional web solutions. Our team is made up of
+          experts in web development, creative designers and professionals who
+          are passionate about the digital world. Nicolás Massaccesi - Frontend
+          Developer With 2 years of experience in web development, Nicolás
+          Massaccesi is our technical leader and expert in all areas of web
+          development. His deep knowledge of programming languages and
+          meticulous approach ensure that each project is executed with
+          precision and efficiency. Juan I. García - Frontend Developer With 2
+          years of experience in web development, Juan I. García is our
+          technical leader and expert in all areas of web development. His deep
+          knowledge of programming languages and meticulous approach ensure that
+          each project is executed with precision and efficiency.
+        </p>
+      </div>
+      <div className="ourTeamSection__box2">
+        <img
+          className="ourTeamSection__img1"
+          src="https://res.cloudinary.com/dwavbmsb2/image/upload/v1684870874/977337284d108765cc0957631f768e10_air9aw.jpg"
+          alt="Foto Nico Massaccesi"
+        />
+      </div>
+      <div className="ourTeamSection__box3">
+        <img
+          className="ourTeamSection__img2"
+          src="https://res.cloudinary.com/dwavbmsb2/image/upload/v1684870875/b425762e454989f9014bfb463fac91b0_tsc26z.png"
+          alt="Foto Juani Garcia"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default OurTeam;
